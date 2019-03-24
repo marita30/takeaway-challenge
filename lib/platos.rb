b768a2f26916dabea683e77cf261f992a7a51c33
M.J <marita.jose.ch@gmail.com>
2019-03-24 20:11:56 +0100

    Historia de usuario#1, agregando la lista de platos con precios

---
+++ lib/platos.rb
@@ -0,0 +1,15 @@
+class Plato
+	attr_reader :platos
+
+	def initialize(list_plato)
+		@platos = []
+		@platos.push(list_plato)
+	end
+
+	def list_platos
+	@platos 
+
+	end
+
+end
+	
---
+++ spec/platos_spec.rb
@@ -0,0 +1,16 @@
+require "platos"
+describe Plato do
+	let(:platos) { platos = {'carne asada with tajadas' => 100, 
+				'tajadas con queso' => 55,
+				'Maduro con queso' => 60,
+				'pollo con tajadas' => 110,
+				'cerdo con tajadas' => 100
+			} }
+
+	describe '#list_platos' do
+		it 'list of platos with price' do
+			orden = Plato.new(platos)
+			expect(orden.list_platos).to eq ([platos])
+		end	
+	end		
+end
